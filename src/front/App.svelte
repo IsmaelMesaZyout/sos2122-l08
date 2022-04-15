7afbc8625889a4d7496b3c4f45709029bfca4c31
Ismael <ismaelmesazyout@gmail.com>
2022-04-15 13:05:26 +0200

    updated code

---
+++ src/front/App.svelte
@@ -0,0 +1,17 @@
+<script>
+	import Router from "svelte-spa-router";
+	import ContactsTable from "./ContactsTable.svelte";
+	import Home from "./Home.svelte";
+	import EditContact from "./EditContact.svelte"
+	const routes = {
+		"/": Home,
+		"/contactList": ContactsTable,
+		"/contact/:contactName": EditContact
+	};
+</script>
+
+<main>
+	<h1>Contact Manager</h1>
+	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<Router {routes} />
+</main>
--- src/front/main.js
+++ src/front/main.js
@@ -1,10 +1,7 @@
 import App from './App.svelte';
 
 const app = new App({ //objeto creado a partir de la clase svelte
-	target: document.body,
-	props: {
-		name: 'world'
-	}
+	target: document.body
 });
 
 export default app;
